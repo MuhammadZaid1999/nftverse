dfa1755280569a94a7ee3107fd81c00a163aacf3
MuhammadZaid1999 <57443175+MuhammadZaid1999@users.noreply.github.com>
2023-05-30 16:56:33 +0500

    update user info

--- code/src/Component/navbar/Username.js
+++ code/src/Component/navbar/Username.js
@@ -3,10 +3,17 @@ import { useStore } from "../../context/GlobalState";
 
 export default function Username(){
     const [{user_data}] = useStore();
+   
     return (
         <div>
-            <h4 className="text-right" style={{marginRight:"1%", marginTop: "-3%"}}>Hello, {user_data.username}!!!</h4>
-            <hr/>
-        </div>        
+        {
+            JSON.stringify(user_data) !== '{}' ? 
+            <div className="text-right" style={{marginRight:"1%", marginTop: "-3%"}}>
+                <h5><b>Hello, {user_data.username}!!!</b></h5>
+                <b>Wallet: {user_data.wallet_address.slice(0,5)}...........{user_data.wallet_address.slice(37,42)}</b>
+            </div> : null
+        }   
+        <hr/>     
+        </div>
     )
 }
